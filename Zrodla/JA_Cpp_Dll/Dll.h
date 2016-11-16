c9ac72882fb767f28ae9af7961cdcda067dc4171
DominikLug <dominik.dymino@gmail.com>
2016-11-16 16:05:32 +0100

    Revert "Pierwszy commit"
    This reverts commit 60866acfa225407a123ed96050f07379ecd2e6dc.

--- Zrodla/JA_Cpp_Dll/Dll.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-class MyClass {
-public:
-	static __declspec(dllexport) void Foo( double **tabA, double **tabB, double **tabC, int startX, int stopX, int maxX, int maxY, int maxZ,bool *endOfThread);
-
-};
