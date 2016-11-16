60866acfa225407a123ed96050f07379ecd2e6dc
DominikLug <dominik.dymino@gmail.com>
2016-11-16 15:58:57 +0100

    Pierwszy commit
    Dodanie plikó źródłowych. Gotowa dll-ka w c++, program główny wymaga
    drobnych poprawek, dll-ka w asemblerze podpięta ale nie funkcjonująca

---
+++ Zrodla/JA_Asm_Dll/Dll.asm
@@ -0,0 +1,11 @@
+.code
+
+Foo PROC
+
+	mov rax, 5
+	add rax, rax
+	ret
+
+Foo endp
+
+end
---
+++ Zrodla/JA_Cpp_Dll/Dll.h
@@ -0,0 +1,6 @@
+#pragma once
+class MyClass {
+public:
+	static __declspec(dllexport) void Foo( double **tabA, double **tabB, double **tabC, int startX, int stopX, int maxX, int maxY, int maxZ,bool *endOfThread);
+
+};
